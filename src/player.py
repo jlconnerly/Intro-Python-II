c86dc3ec2863c154113d583939eb96c210b65cd9
Jake <jlconnerly@gmail.com>
2020-02-12 23:14:09 -0600

    Added a move list to help player

--- src/player.py
+++ src/player.py
@@ -17,6 +17,11 @@ class Player():
     def pick_up_item(self, item):
         self.inventory.append(item)
         print(f"{self.name} picked up {item.name}")
+    def drop_item(self, item_name):
+        for i in self.inventory:
+            if item_name == i.name:
+                self.inventory.remove(i)
+        print(f"{self.name} dropped {i.name}")
 
 class Item():
     def __init__(self, weight, hp, name, catagory):
@@ -24,10 +29,19 @@ class Item():
         self.hp = hp
         self.name = name
         self.catagory = catagory
-    
+
+player_choice = input("What do you want to do? (type 'move list' to see list. Otherwise enter move)")
+
+if player_choice == "move list":
+    print("Press 'N' to move North\nPress 'S' to move South\nYou get the picture...\nType 'open inv' to view inventory\nType 'drop [item]' to drop an item\nType pickup [item] to pick up item\n\n")
+player_choice = input("What do you want to do? (type 'move list' to see list. Otherwise enter move)")
 uga = Player("Orc", "Heavy", "Uga", [])
 
 uga.move("north")
 uga.pick_up_item(Item(5, 12, "Common Shield", "armor"))
+uga.pick_up_item(Item(2, 0, "Lesser Soul Gem", "Magic"))
+uga.pick_up_item(Item(1, 0, "Tomato", "Food"))
 
+uga.inventory_check()
+uga.drop_item("Tomato")
 uga.inventory_check()
